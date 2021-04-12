1f957a5406927880751dd70f89c1fe14db54ee3e
emusisi4 <emusisi4@gmail.com>
2021-04-12 16:32:42 +0300

    ella commit

--- resources/assets/js/components/Sales.vue
+++ resources/assets/js/components/Sales.vue
@@ -198,7 +198,7 @@ button.add-newm {
                   <label>Product Name / Code</label>
                     <select name ="productcode" v-model="form.productcode" tabindex="1" id ="select2"   :class="{'is-invalid': form.errors.has('productcode')}">
                     <option value=" ">  </option>
-                    <option v-for='data in companyproducts' v-bind:value='data.id'> {{ data.name }}</option>
+                    <option v-for='data in companyproducts' v-bind:value='data.id'> {{ data.name }} </option>
 
                     </select>
              
